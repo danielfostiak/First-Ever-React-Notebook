b90e016352c30125325e5eeafff2a77c46951d12
danielfostiak <fostiakd@gmail.com>
2021-08-06 21:43:17 +0100

    added localStorage in a booky way

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-This is my notes clone using react
-This was primarily to solidify my react knowledge using no copied code
-Probably not the most optimized but it is react so I am happy
+This is my notes clone using react.
+This was primarily to solidify my react knowledge using no copied code.
+Probably not the most optimized but it is react so I am happy.
 Nice.
--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { useState } from "react";
 import AddNote from "./components/AddNote";
+import Note from "./components/Note";
 import NoteList from "./components/NoteList";
 
+if (!localStorage.getItem("noteList")) {
+  localStorage.setItem("noteList", "[]");
+}
+
 const App = () => {
-  const [noteList, setNoteList] = useState([]);
+  const [noteList, setNoteList] = useState(
+    JSON.parse(localStorage.getItem("noteList"))
+  );
   const getInfo = (info) => {
+    localStorage.setItem("noteList", JSON.stringify(noteList));
+    console.log(localStorage.getItem("noteList"));
     setNoteList(() => [...noteList, info]);
   };
 
